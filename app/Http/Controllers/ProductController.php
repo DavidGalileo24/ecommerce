f442c097ed6fa1a6bb92154a5f86c52ff639a6d9
DavidGalileo24 <david.salgado.mejia@dive.digital>
2025-02-03 02:39:04 -0600

    Test webhook stripe

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,8 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class ProductController extends Controller
 {
-    //
+    public function test(Request $request){
+        try {
+            Log::info(json_encode($request->all()));
+        } catch (\Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ]);
+        }
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,8 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/webhook',
+            'stripe/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
